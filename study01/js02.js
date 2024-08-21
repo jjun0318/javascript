66ccb5549025ea75fc5da8932a2c1f5e908eea69
jjun0318 <sszz7079@naver.com>
2024-08-21 17:17:39 +0900

    자바스크립트 수업

---
+++ study01/js02.js
@@ -0,0 +1,6 @@
+// html 파일에 있는 태그 중 지정한 선택자와 같은 태그를 검색
+var btn3 = document.querySelector("#btn3");
+// 검색한 태그에 클릭 이벤트를 적용
+btn3.addEventListener("click", function() {
+    alert("3번 방식으로 자바스크립트 사용");
+});
